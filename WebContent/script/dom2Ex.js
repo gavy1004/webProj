b028cd374cc8c29d49e304c3d0a5288facfb6a00
admin <admin@YD02-16>
2021-04-20 17:48:27 +0900

    good bye

---
+++ WebContent/script/dom2Ex.js
@@ -0,0 +1,142 @@
+function createTitle() {
+    var trTag = document.createElement('tr');
+    for (var i = 0; i < arguments.length; i++) {
+        var td1 = document.createElement('th');
+        td1.innerHTML = arguments[i];
+        trTag.appendChild(td1);
+    }
+
+    tbl.appendChild(trTag);
+}
+
+function createData() {
+    for (var person of persons) {
+        var trTag = document.createElement('tr');
+        trTag.setAttribute('id', person.id);
+        trTag.onmouseover = mouseOverFnc;
+        trTag.onmouseout = mouseOutFnc;
+
+        for (var field in person) { // for ~ in 필드의 요소 반복
+            if (field == 'id') {
+                var tdTag = document.createElement('td');
+                tdTag.onclick = modifyFunc;
+
+                var text = document.createTextNode(person[field]);
+                trTag.appendChild(tdTag);
+                tdTag.appendChild(text);
+            }else if(field == 'name'){
+                var tdTag =document.createElement('td');
+                var link = document.createElement('a');
+                link.setAttribute('href','dom.jsp?name=Hong&id=user1&score=80&gender=여');
+
+                //('dom.jsp?name='+person.name+'&id='+person.id+'&score='+person.score+'&gender='+person.gender);
+                link.innerHTML= person.name;
+                tdTag.appendChild(link);
+                trTag.appendChild(tdTag);
+            } else {
+                var tdTag = document.createElement('td');
+                var text = document.createTextNode(person[field]);
+                trTag.appendChild(tdTag);
+                tdTag.appendChild(text);
+            }
+
+        }
+        var btn = document.createElement('button');
+        btn.innerHTML = '삭제';
+        btn.onclick = deleteRow;
+
+        var tdTag = document.createElement('td');
+        tdTag.appendChild(btn);
+        trTag.appendChild(tdTag);
+
+        tbl.appendChild(trTag);
+    }
+}
+
+function mouseOverFnc() {
+    this.style.backgroundColor = "yellow";
+}
+
+function mouseOutFnc() {
+    this.style.backgroundColor = '';
+
+}
+
+function deleteRow() {
+    this.parentNode.parentNode.remove();
+}
+
+
+function modifyFunc() {
+
+    console.log(this);
+    var idVal = this.innerHTML;
+    var nameVal = this.previousSibling.innerHTML;
+    var scoreVal = this.nextSibling.innerHTML;
+    var genVal = this.parentNode.childNodes[3].innerHTML;
+    console.log(idVal, nameVal, scoreVal, genVal);
+
+    document.getElementById('name').value = nameVal;
+    document.getElementById('id').value = idVal;
+    document.getElementById('score').value = scoreVal;
+    var genders = document.querySelectorAll('input[name="gender"]');
+    for (var i = 0; i < genders.length; i++) {
+        if (genders[i].value == genVal) {
+            genders[i].checked = true;
+        }
+    }
+}
+function saveBtnFnc() {
+    var iName = document.getElementById('name');
+    var iId = document.querySelector('input[name="id"]');
+    var iScore = document.getElementsByTagName('input')[2];
+    var iGender = document.querySelector('input[name="gender"]:checked');
+
+    var tr = document.createElement('tr');
+    tr.setAttribute('align', 'center'); //속성
+    tr.onmouseover = mouseOverFnc; // 마우스를 올리면 배경색 라벤더
+    tr.onmouseout = mouseOutFnc; // 마우스를 내리면 배경색 없음
+
+    // name
+    var td = document.createElement('td');
+    td.innerHTML = iName.value;
+    tr.appendChild(td);
+
+    // id
+    var td = document.createElement('td');
+    td.innerHTML = iId.value;
+    tr.appendChild(td);
+
+    // score
+    var td = document.createElement('td');
+    td.innerHTML = iScore.value;
+    tr.appendChild(td);
+
+    // gender
+    var td = document.createElement('td');
+    td.innerHTML = iGender.value;
+    tr.appendChild(td);
+
+    //button
+    var td1 = document.createElement('td'); // button이 들어갈 새로운 td추가
+    var buttons = document.createElement('button'); // button 생성
+    buttons.innerHTML = "삭제"; // button Label 넣어주기
+
+    buttons.onclick = deleteRow; // button 클릭 시 삭제
+
+    td1.appendChild(buttons); // td1에 button 넣어주기
+    tr.appendChild(td1); // tr에 td1 추가
+    tbl.appendChild(tr);
+}
+
+function modifyBtnFunc() {
+    var id = document.getElementById('id').value;
+    var targetTr = document.getElementById(id);
+    console.log(targetTr);
+    targetTr.children[0].innerHTML = document.getElementById('name').value;
+    targetTr.children[2].innerHTML = document.getElementById('score').value;
+    targetTr.children[3].innerHTML = document.querySelector('input[name="gender"]:checked').value;
+    
+
+    
+}
